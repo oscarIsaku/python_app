953ff1446d22bcc8bafcb497f1fddea904b1d9a9
isaac <tasacata_sorte@yahoo.co.jp>
2020-05-09 18:38:41 +0900

    メッセージ記述位置設定

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,11 +1,14 @@
 import click
+from lgtm.drawer import save_with_message
+from lgtm.image_source import get_image
 
 @click.command()
 @click.option('--message','-m',default='LGTM',show_default=True,help='画像に乗せる文字列')
 @click.argument('keyword')
 def cli(keyword,message):
   lgtm(keyword,message)
-  click.echo('lgtm')
+  # click.echo('lgtm')
 
 def lgtm(keyword,message):
-  pass
+  with get_image(keyword) as fp:
+    save_with_message(fp,message)
